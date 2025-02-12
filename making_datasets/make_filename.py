2e269772733a88573317dac4ca7d8aeecf7b7c66
ColdTbrew <shchoi8687@hotmail.com>
2025-02-12 16:21:47 +0900

    make_photo_todo

--- making_datasets/make_filename.py
+++ making_datasets/make_filename.py
@@ -9,23 +9,21 @@ woman_bottom = ['womana','womanb','womanc','womand','womane','womanf']
 angle = ['30', '60', '90', '120', '150' ]
 
 man_count = 0
-with open('dataset_to_take.csv', 'w', newline='') as csvfile:
+with open('dataset_to_take_2.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(['filename','조합명' ,'조합의 촬영 순서'])
     for man in man_people:
-        for top in man_top:
-            for bottom in man_bottom:
-                for ang in angle:
-                    filename = f'{man}_{top}_{bottom}_{ang}.jpg'
-                    combination_name = f'{top}_{bottom}'
-                    writer.writerow([filename, combination_name, None])
-                    man_count += 1
+        for top, bottom in zip(man_top, man_bottom):
+            for ang in angle:
+                filename = f'{man}_{top}_{bottom}_{ang}.jpg'
+                combination_name = f'{top}_{bottom}'
+                writer.writerow([filename, combination_name, None])
+                man_count += 1
     print("man_count :", man_count, end='\n')
 
     woman_count = 0
     for man in woman_people:
-        for top in woman_top:
-            for bottom in woman_bottom:
+        for top, bottom in zip(woman_top, woman_bottom):
                 for ang in angle:
                     filename = f'{man}_{top}_{bottom}_{ang}.jpg'
                     combination_name = f'{top}_{bottom}'
@@ -33,3 +31,13 @@ with open('dataset_to_take.csv', 'w', newline='') as csvfile:
                     woman_count += 1
     print("woman_count :", woman_count)
 
+with open('dataset_cloth.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['filename','is_taken'])
+    for top in [man_top, woman_top]:
+        for cloth in top:
+            writer.writerow([cloth, None])
+
+    for bottom in [man_bottom, woman_bottom]:
+        for cloth in bottom:
+            writer.writerow([cloth, None])
